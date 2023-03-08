a7e463e5c132533a07d56fc9a19e633b3446e5d8
Rangboo <93071040+Rangboo@users.noreply.github.com>
2023-03-08 20:14:21 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1="";
 song2="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 
 function setup()
 {
@@ -7,6 +11,9 @@ function setup()
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw()
@@ -23,4 +30,26 @@ function preload()
 function play()
 {
     song.play();
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+
+    }
 }
